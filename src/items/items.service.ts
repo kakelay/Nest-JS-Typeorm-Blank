a1a0da68e9ba30eec78313771153b2274fc485ba
kakelay <kakelay18052002@gmail.com>
2023-12-13 12:53:35 +0700

    finish  transaction with throw  error exception

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -47,13 +47,26 @@ export class ItemsService {
   }
 
   async update(id: number, updateItemDto: UpdateItemDto) {
-    const item = await this.itemsRepository.findOneBy({ id });
-    item.public = updateItemDto.public;
-    const comments = updateItemDto.comments.map(
-      (CreateCommentDto) => new Comment(CreateCommentDto),
-    );
-    item.commnnets = comments;
-    await this.entityManager.save(item);
+    // const item = await this.itemsRepository.findOneBy({ id });
+    // item.public = updateItemDto.public;
+    // const comments = updateItemDto.comments.map(
+    //   (CreateCommentDto) => new Comment(CreateCommentDto),
+    // );
+    // item.commnnets = comments;
+    // await this.entityManager.save(item);
+    await this.entityManager.transaction(async (entityManager) => {
+      const item = await this.itemsRepository.findOneBy({ id });
+      item.public = updateItemDto.public;
+      const comments = updateItemDto.comments.map(
+        (CreateCommentDto) => new Comment(CreateCommentDto),
+      );
+      item.commnnets = comments;
+      await entityManager.save(item);
+      throw new Error();
+      const tagContent = `${Math.random()}`;
+      const tag = new Tag({ content: tagContent });
+      await entityManager.save(tag);
+    });
   }
 
   async remove(id: number) {
